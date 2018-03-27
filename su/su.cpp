53a50fdb1dcb5dc9d31d3e3e542bbc5ebfa15a26
Gary Bisson <gary.bisson@boundarydevices.com>
2018-03-27 19:32:00 +0200

    su: add BOARD_SU_ALLOW_ALL option
    This isn't recommended, but in case someone needs su to be accessible
    by all apps, simply add the following in BoardConfig.mk:
    BOARD_SU_ALLOW_ALL := true
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- su/su.cpp
+++ su/su.cpp
@@ -81,10 +81,11 @@ void extract_uidgids(const char* uidgids, uid_t* uid, gid_t* gid, gid_t* gids, i
 }
 
 int main(int argc, char** argv) {
+#ifndef SU_ALLOW_ALL
     uid_t current_uid = getuid();
     if (current_uid != AID_ROOT && current_uid != AID_SHELL && current_uid != AID_SYSTEM)
         error(1, 0, "not allowed");
-
+#endif
     // Handle -h and --help.
     ++argv;
     if (*argv && (strcmp(*argv, "--help") == 0 || strcmp(*argv, "-h") == 0)) {
